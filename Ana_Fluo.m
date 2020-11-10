b8169beab72d399dd918bc4afeeea39aec3f7b5b
SamFB <samuel.belanger@polymtl.ca>
2020-11-10 15:58:59 -0500

    Multiple fluo acquisitions management.

--- Ana_Fluo.m
+++ Ana_Fluo.m
@@ -7,19 +7,14 @@ if( ~strcmp(FolderName(end), filesep) )
     FolderName = strcat(FolderName, filesep);
 end
 
-fprintf('Opening files.\n');
 
-FileList = dir([FolderName 'Data_Fluo.mat']);
+
+FileList = dir([FolderName '*Fluo*.mat']);
 if( isempty(FileList) )
     disp(['No Fluorescence data files found in ' FolderName ' Folder.']);
     disp('Fluorescence Analysis will not run');
     return;
 end
-
-FInfo =  matfile([FolderName 'Data_Fluo.mat'], 'Writable', true);
-fid = fopen([FolderName 'fChan.dat']);
-Fluo = fread(fid, inf, 'single=>single');
-fclose(fid);
 
 if( exist([FolderName 'Data_Hbs.mat'], 'file') && b_HbCorr )
     FluoCorrGen(FolderName);
@@ -29,57 +24,66 @@ if( exist([FolderName 'Data_Hbs.mat'], 'file') && b_HbCorr )
     Corr = fread(fid, inf, 'single');
     Corr = single(Corr);
     fclose(fid);
-
-    fprintf('Applying HB Correction.\n');
-    
-    Fluo = Fluo(1:length(Corr));
-    Fluo = Fluo.*Corr;
 end
 
-Fluo = reshape(Fluo, FInfo.datSize(1,1), FInfo.datSize(1,2),[]);
-FInfo.datLength = size(Fluo,3);
+for indF = 1:size(FileList,1)
+    FInfo =  matfile([FileList(indF).folder filesep FileList(indF).name], 'Writable', true);
+    fprintf(['Opening file: ' cell2mat(FInfo.Wavelength(1,1)) 'nm.\n']);
+    fid = fopen([FolderName 'fChan_' cell2mat(FInfo.Wavelength(1,1)) '.dat']);
+    Fluo = fread(fid, inf, 'single=>single');
+    fclose(fid);
 
-fprintf('Filtering.\n');
-if( ~b_HilbertF )
-    dims = size(Fluo);
-    
-    %Filter parameters
-    f = fdesign.lowpass('N,F3dB', 4, FInfo.Freq/2, FInfo.Freq);
-    hpass = design(f,'butter');
-    f = fdesign.lowpass('N,F3dB', 4, 1/10, FInfo.Freq);
-    lpass = design(f,'butter');
-    
-    %Memory management
-    tSiz = (prod(dims)*8)/1e9;
-    tSiz = (16 + 8*tSiz);
-    nbStep = ceil(tSiz / 32);
-    
-    lims = round(linspace(1, dims(1)*dims(2) + 1, nbStep+1));
-    Fluo = reshape(Fluo, [], dims(3));
-    for indL = 2:length(lims)
-        imin = lims(indL - 1);
-        imax = lims(indL) - 1;
-        dFh = single(filtfilt(hpass.sosMatrix, hpass.ScaleValues, double(Fluo(imin:imax,:)')))';
-        dFl = single(filtfilt(lpass.sosMatrix, lpass.ScaleValues, double(Fluo(imin:imax,:)')))';
-        Fluo(imin:imax,:) = (dFh - dFl)./dFl;
-        clear dFh dFl;
+    if( exist([FolderName 'Data_Hbs.mat'], 'file') && b_HbCorr )        
+        fprintf('Applying HB Correction.\n');
+        
+        Fluo = Fluo(1:length(Corr));
+        Fluo = Fluo.*Corr;
     end
     
-    Fluo  = reshape(Fluo, dims(1), dims(2), dims(3));
-else
-    dims = size(Fluo);
-    [~, ylower] = envelope(reshape(Fluo,[],dims(3))',4*FInfo.Freq,'peak');
-    ylower = reshape(ylower', dims);
-    Fluo = (Fluo - ylower)./ylower;
-    for ind = 1:dims(3)
-        Fluo(:,:,ind) = medfilt2(squeeze(Fluo(:,:,ind)),[5 5],'symmetric');
+    Fluo = reshape(Fluo, FInfo.datSize(1,1), FInfo.datSize(1,2),[]);
+    FInfo.datLength = size(Fluo,3);
+    
+    fprintf('Filtering.\n');
+    if( ~b_HilbertF )
+        dims = size(Fluo);
+        
+        %Filter parameters
+        f = fdesign.lowpass('N,F3dB', 4, FInfo.Freq/2, FInfo.Freq);
+        hpass = design(f,'butter');
+        f = fdesign.lowpass('N,F3dB', 4, 1/10, FInfo.Freq);
+        lpass = design(f,'butter');
+        
+        %Memory management
+        tSiz = (prod(dims)*8)/1e9;
+        tSiz = (16 + 8*tSiz);
+        nbStep = ceil(tSiz / 32);
+        
+        lims = round(linspace(1, dims(1)*dims(2) + 1, nbStep+1));
+        Fluo = reshape(Fluo, [], dims(3));
+        for indL = 2:length(lims)
+            imin = lims(indL - 1);
+            imax = lims(indL) - 1;
+            dFh = single(filtfilt(hpass.sosMatrix, hpass.ScaleValues, double(Fluo(imin:imax,:)')))';
+            dFl = single(filtfilt(lpass.sosMatrix, lpass.ScaleValues, double(Fluo(imin:imax,:)')))';
+            Fluo(imin:imax,:) = (dFh - dFl)./dFl;
+            clear dFh dFl;
+        end
+        
+        Fluo  = reshape(Fluo, dims(1), dims(2), dims(3));
+    else
+        dims = size(Fluo);
+        [~, ylower] = envelope(reshape(Fluo,[],dims(3))',4*FInfo.Freq,'peak');
+        ylower = reshape(ylower', dims);
+        Fluo = (Fluo - ylower)./ylower;
+        for ind = 1:dims(3)
+            Fluo(:,:,ind) = medfilt2(squeeze(Fluo(:,:,ind)),[5 5],'symmetric');
+        end
     end
-end
-
-fprintf('Saving.\n');
-fid = fopen([FolderName 'fChan.dat'],'w');
-fwrite(fid,Fluo,'single');
-fclose(fid);
+    
+    fprintf('Saving.\n');
+    fid = fopen([FolderName 'fChan_' cell2mat(FInfo.Wavelength(1,1)) '.dat'],'w');
+    fwrite(fid,Fluo,'single');
+    fclose(fid);
 
 end
 
