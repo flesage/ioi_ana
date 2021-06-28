5eea12dead8480d9b00096b1af389a20327571b1
SamFB <samuel.belanger@labeotech.ca>
2021-06-28 12:26:47 -0400

    Compatibility issues.

--- ConvertToTiff.m
+++ ConvertToTiff.m
@@ -4,13 +4,50 @@ if( ~strcmp(FolderPath(end),filesep) )
     FolderPath = strcat(FolderPath, filesep);
 end
 disp(['Starting Tiff conversion for: ' FolderPath]);
-ChanList = dir([FolderPath '*Chan.dat']);
-InfList = dir([FolderPath 'Data_*.mat']);
+ChanList = dir([FolderPath '*.dat']);
+if( isempty(ChanList) )
+    disp(['No dat file found. End of convertion.']);
+    return;
+end
+
+if( exist([FolderPath 'red.mat'],'file') )
+    InfList = dir([FolderPath 'red.mat']);
+end
+if( exist([FolderPath 'green.mat'],'file') )
+    if( ~exist('InfList') )
+        InfList = dir([FolderPath 'green.mat']);
+    end
+    InfList(end+1) = dir([FolderPath 'green.mat']);
+end
+if( exist([FolderPath 'yellow.mat'],'file') )
+    if( ~exist('InfList') )
+        InfList = dir([FolderPath 'yellow.mat']);
+    end
+    InfList(end+1) = dir([FolderPath 'yellow.mat']);
+end
+if( exist([FolderPath 'fluo*.mat'],'file') )
+    fluoList = dir([FolderPath 'fluo*.mat']);
+    for ind = 1:size(fluoList,2)
+        if( ~exist('InfList') )
+            InfList = dir([FolderPath fluoList(ind).name]);
+        end
+        InfList(end+1) = dir([FolderPath fluoList(ind).name]);
+    end
+end
+if( isempty(InfList) )
+    InfList = dir([FolderPath 'Data_*.mat']);
+end
+if( isempty( InfList ) )
+    disp(['No mat file found. End of convertion.']);
+    return;
+end
+
 for indC = 1:size(ChanList,1)
     ChanName = ChanList(indC).name;
     fid = fopen([FolderPath ChanName]);
     dat = fread(fid,inf,'single');
-    idx = arrayfun(@(X) contains(InfList(X).name,['_' ChanName(1)],'IgnoreCase',true), 1:size(InfList,1)); 
+    tag = ChanName(1:(strfind(ChanName,'.') - 1));
+    idx = arrayfun(@(X) contains(InfList(X).name, tag,'IgnoreCase',true), 1:size(InfList,2)); 
     Infos = matfile([FolderPath InfList(find(idx)).name]);
     dat = reshape(dat, Infos.datSize(1,1), Infos.datSize(1,2), []);
     
