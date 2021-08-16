acedc35f7d60e0b707f4bc6b238a6b80d137fa57
SamFB <samuel.belanger@labeotech.ca>
2021-08-16 11:59:15 -0400

    Correction to be compatible with the new names of the files.

--- ConvertToMat.m
+++ ConvertToMat.m
@@ -1,4 +1,4 @@
-function ConvertToMat(folderPath)
+function ConvertToMat(folderPath, bSplitConditions)
 
 if( ~strcmp(folderPath(end), filesep) )
     folderPath = strcat(folderPath, filesep);
@@ -7,12 +7,29 @@ end
 disp(['Starting .mat conversion for: ' folderPath]);
 ChanList = dir([folderPath '*Chan.dat']);
 InfList = dir([folderPath 'Data_*.mat']);
+if( isempty(ChanList) )
+   ChanList = dir([folderPath '*.dat']);
+   Tags = {'red', 'green', 'yellow', 'fluo', 'speckle'};
+   idx = arrayfun(@(x) contains(ChanList(x).name, Tags), 1:size(ChanList,1));
+   ChanList = ChanList(idx);
+   
+   InfList = dir([folderPath '*.mat']);
+   idx = arrayfun(@(x) startsWith(InfList(x).name, Tags), 1:size(InfList,1));
+   InfList = InfList(idx);
+   
+   if( isempty(ChanList) )
+       disp('Error: No data to convert was found');
+       return;
+   end
+end
+
 for indC = 1:size(ChanList,1)
     ChanName = ChanList(indC).name;
     fid = fopen([folderPath ChanName]);
     dat = fread(fid,inf,'single');
-    idx = arrayfun(@(X) contains(InfList(X).name,['_' ChanName(1)],'IgnoreCase',true), 1:size(InfList,1)); 
-    Infos = matfile([folderPath InfList(find(idx)).name]);
+    idx = arrayfun(@(X) contains(InfList(X).name,ChanName(1:(strfind(ChanName, '.')-1)),...
+        'IgnoreCase',true), 1:size(InfList,1)); 
+    Infos = matfile([folderPath InfList(idx).name]);
     dat = reshape(dat, Infos.datSize(1,1), Infos.datSize(1,2), []);
     Stim = Infos.Stim;
     
@@ -30,25 +47,32 @@ for indC = 1:size(ChanList,1)
         case 'f'
             disp('Saving fluo channel.');
             outFName = strcat(outFName, 'Fluo.mat');
+        case 's'
+            disp('Saving speckle channel.');
+            outFName = strcat(outFName, 'Speckle.mat');
     end
     F = matfile(outFName, 'Writable', true);
     
-    idCond = nonzeros(unique(Stim));
-    nbCond = length(idCond);
-    for indS = 1:nbCond
-        iStim = idCond(indS) == Stim;
-        iS = find(diff(iStim)>0) + 1;
-        iE = find(diff(iStim)<0) + 1;
-        if( Stim(1) == idCond(indS) )
-            iS = cat(1, 1, iS);
+    if( bSplitConditions )
+        idCond = nonzeros(unique(Stim));
+        nbCond = length(idCond);
+        for indS = 1:nbCond
+            iStim = idCond(indS) == Stim;
+            iS = find(diff(iStim)>0) + 1;
+            iE = find(diff(iStim)<0) + 1;
+            if( Stim(1) == idCond(indS) )
+                iS = cat(1, 1, iS);
+            end
+            iL = min(iE - iS);
+            Cond = zeros(size(dat,1),size(dat,2),iL,length(iS));
+            for indR = 1:length(iS)
+                Cond(:,:,:,indR) = dat(:,:,iS(indR) + (0:(iL-1)));
+            end
+            eval(['F.Cond' int2str(idCond(indS)) ' = Cond;'])
         end
-        iL = min(iE - iS);
-        Cond = zeros(size(dat,1),size(dat,2),iL,length(iS));
-        for indR = 1:length(iS)
-           Cond(:,:,:,indR) = dat(:,:,iS(indR) + (0:(iL-1))); 
-        end
-        eval(['F.Cond' int2str(idCond(indS)) ' = Cond;'])
-    end 
+    else
+        F.Cond0 = dat;
+    end
 end
 
 disp('Mat conversion is done.')
