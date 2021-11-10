49cf1ceeb4a709760fe70b1d7a3ad9929ae3622e
SamFB <samuel.belanger@labeotech.ca>
2021-11-10 13:02:57 -0500

    Added some functionalities and some error management to the function

--- SpeckleMapping.m
+++ SpeckleMapping.m
@@ -1,4 +1,4 @@
-function DatOut = SpeckleMapping(folderPath, sType, channel)
+function DatOut = SpeckleMapping(folderPath, sType, channel, bSave, bLogScale)
 %%%%%%%%%%%%%%%%%%%% Speckle Mapping function %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Show the standard deviation (spatialy or temporaly) of speckle
 % acquisition. This measure is proportional to the strength of blood flow
@@ -21,12 +21,15 @@ function DatOut = SpeckleMapping(folderPath, sType, channel)
 
 if(nargin < 3)
     channel = 'speckle';
+    bSave = 1;
+    bLogScale = 1;
 end
 
 if( ~strcmp(folderPath(end), filesep) )
     folderPath = strcat(folderPath, filesep);
 end
 
+channel = lower(channel);
 if(~exist([folderPath channel '.dat'],'file') )
     disp([channel '.dat file is missing. Did you run ImagesClassificiation?']);
     return;
@@ -34,46 +37,52 @@ end
 
 disp(['Opening ' channel '.dat']);
 
-Infos = matfile([folderPath channel '.mat']);
-fid = fopen([folderPath channel '.dat']);
-dat = fread(fid, inf, '*single');
-dat = reshape(dat, Infos.datSize(1,1), Infos.datSize(1,2),[]);
-dat = dat./mean(dat,3);
-
-disp('Mapping Computation');
-switch sType
-    case 'Spatial'
-        Kernel = zeros(5,5,1);
-        Kernel(:,:,1) = fspecial('disk',2)>0;
-        DatOut = stdfilt(dat,Kernel);
-    case 'Temporal'
-        Kernel = ones(1,1,5);
-        
-        DatOut = stdfilt(dat,Kernel);
-     
+try
+    Infos = matfile([folderPath channel '.mat']);
+    fid = fopen([folderPath channel '.dat']);
+    dat = fread(fid, inf, '*single');
+    dat = reshape(dat, Infos.datSize(1,1), Infos.datSize(1,2),[]);
+    dat = dat./mean(dat,3);
+catch 
+    disp(['Failed to open ' channel ' files'])
+    return
 end
 
-DatOut = -log10(DatOut);
+disp('Mapping Computation');
+switch lower(sType)
+    case 'spatial'
+        Kernel = zeros(5,5,1,'single');
+        Kernel(:,:,1) = single(fspecial('disk',2)>0);
+        DatOut = stdfilt(dat,Kernel);
+    case 'temporal'
+        Kernel = ones(1,1,5,'single');
+        DatOut = stdfilt(dat,Kernel);     
+end
+DatOut = single(DatOut);
+
+if( bLogScale )
+    DatOut = -log10(DatOut);
+end
 
 %Remove outliers
-sorted = sort(DatOut(:));
-outlier = sorted(round(0.99*size(sorted(:),1)));
-DatOut(DatOut>outlier) = outlier;
-
-disp('Done');
+pOutlier = prctile(DatOut(:), 99);
+DatOut(DatOut>pOutlier) = pOutlier;
 
 %Generate output
 % copyfile([folderPath channel '.mat'], [folderPath flow '.mat'])
-mFileOut = matfile([folderPath 'flow.mat'], 'Writable', true);
-mFileOut.FirstDim = Infos.FirstDim;
-mFileOut.Freq = Infos.Freq;
-mFileOut.Stim = Infos.Stim;
-mFileOut.datLength = Infos.datLength;
-mFileOut.datSize = Infos.datSize;
-mFileOut.datFile = 'flow.dat';
+if( bSave )
+    disp('Saving');
+    mFileOut = matfile([folderPath 'flow.mat'], 'Writable', true);
+    mFileOut.FirstDim = Infos.FirstDim;
+    mFileOut.Freq = Infos.Freq;
+    mFileOut.Stim = Infos.Stim;
+    mFileOut.datLength = Infos.datLength;
+    mFileOut.datSize = Infos.datSize;
+    mFileOut.datFile = 'flow.dat';
 
-fid = fopen([folderPath 'flow.dat'],'w'); 
-fwrite(fid, single(DatOut), 'single');
-fclose(fid);
-
+    fid = fopen([folderPath 'flow.dat'],'w'); 
+    fwrite(fid, single(DatOut), 'single');
+    fclose(fid);
+end
+disp('Done');
 end
